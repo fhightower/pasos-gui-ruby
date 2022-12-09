bead5a738ed24da85dcd727426fcb1432c01be6b
Floyd Hightower <floyd.hightower27@gmail.com>
2022-12-09 15:20:59 -0500

    Add new and show pages

--- app/controllers/pasos_controller.rb
+++ app/controllers/pasos_controller.rb
@@ -2,4 +2,29 @@ class PasosController < ApplicationController
   def index
     @pasos = Paso.all
   end
+
+  def show
+    @paso = Paso.find(params[:id])
+  end
+
+  def new
+    @paso = Paso.new()
+  end
+
+  def create
+    @paso = Paso.new(paso_params)
+
+    if @paso.save
+      redirect_to @paso
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def paso_params
+    params.require(:paso).permit(:day, :destination)
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "pasos#index"
 
-  get '/pasos', to: "pasos#index"
+  resources :pasos
 end
