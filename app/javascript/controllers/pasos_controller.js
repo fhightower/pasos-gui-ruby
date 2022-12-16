9b479730ad332effecfdc0738d656baab163e7d3
Floyd Hightower <floyd.hightower27@gmail.com>
2022-12-16 13:13:41 -0500

    Add search functionality on index page w/ stimulus

--- app/javascript/controllers/pasos_controller.js
+++ app/javascript/controllers/pasos_controller.js
@@ -1,7 +1,21 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  search() {
-    console.log("Searching", this.element)
-  }
+    static targets = [ "query", "paso" ]
+
+    matches_query(text) {
+        return text.search(this.query) != -1
+    }
+
+    search() {
+        this.pasoTargets.forEach(element => {
+            element.hidden = !this.matches_query(element.textContent)
+        })
+    }
+
+    get query() {
+        return this.queryTarget.value
+    }
+
+
 }
